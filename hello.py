41ea668ad44788f48689a1680d750512459adfa8
eszterkaloz <eszterkaloz@gmail.com>
2016-05-16 10:11:45 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,9 +2,7 @@ import sys
 
 def main(argv):
     if argv == sys.argv[:0]:
-    #if len(sys.argv) == 1:
         print("Hello World!")
-    #if len(sys.argv) > 0:
     else:
         print ("Hello " + sys.argv[1] + "!")
 
